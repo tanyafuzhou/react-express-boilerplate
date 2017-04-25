e0876ea95a6e9561c8e0e6a917bdb4952eb1d695
tanyafuzhou <tanyafuzhou@gmail.com>
2017-04-25 20:04:09 +0800

    :smile:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById("root");
 
-const render = Component =>
-  ReactDOM.render(
-      <Component />,
-    rootEl
-  )
+const render = Component => ReactDOM.render(<Component />, rootEl);
 
-render(App)
-if (module.hot) module.hot.accept()
+render(App);
+if (module.hot) module.hot.accept();
